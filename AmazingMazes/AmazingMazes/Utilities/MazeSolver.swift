a4223f652b14e1338bbd49d090ddaa228a537c34
Zach Vilardell <szvila01@louisville.edu>
2018-02-23 21:51:56 -0500

    added MazeColors struct

--- AmazingMazes/AmazingMazes/Utilities/MazeSolver.swift
+++ AmazingMazes/AmazingMazes/Utilities/MazeSolver.swift
@@ -10,6 +10,14 @@ import UIKit
 
 class MazeSolver: NSObject {
     
+    struct MazeColors {
+        //all maze colors, represented in UIExtendedSRGBColorSpace
+        static let red = UIColor.red
+        static let blue = UIColor.blue
+        static let white = UIColor(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        static let black = UIColor(red: 0.0, green: 0.0, blue: 0.0, alpha: 1.0)
+    }
+    
     typealias SolveMazeCompletion = (Bool, UIImage?)->()
     
     //---------------------------------------------------------------------------------------------------------------------------
@@ -24,13 +32,22 @@ class MazeSolver: NSObject {
     
     func solveMaze(mazeImage: UIImage, completion: SolveMazeCompletion) {
         if let cgImage = mazeImage.cgImage {
-            createColorMatrix(from: cgImage)
+            if let colorMatrix = createColorMatrix(from: cgImage) {
+                print(colorMatrix.count)
+                print(colorMatrix[0].count)
+                print(MazeColors.red)
+                print(MazeColors.blue)
+                print(MazeColors.white)
+                print(MazeColors.black)
+            } else {
+                completion(false, nil)
+            }
         } else {
             completion(false, nil)
         }
     }
     
-    func createColorMatrix(from image: CGImage) {
+    func createColorMatrix(from image: CGImage) -> [[UIColor]]? {
         if let pixelData = image.dataProvider?.data {
             let bytesPerPixel: Int = image.bitsPerPixel / 8
             let pixelBytes: UnsafePointer<UInt8> = CFDataGetBytePtr(pixelData)
@@ -50,20 +67,8 @@ class MazeSolver: NSObject {
                 }
                 colorMatrix.append(colorRow)
             }
-            print(colorMatrix.count)
-            print(colorMatrix[0].count)
-            let blue = colorMatrix[image.height - 5][5]
-            print(blue)
-            let red = colorMatrix[image.height - 5][(image.width / 2) + 7]
-            print(red)
-            let origin = colorMatrix[0][0] //black
-            print(origin)
-            let one = colorMatrix[1][1] //white
-            print(one)
-            print(blue == UIColor.blue)
-            print(red == UIColor.red)
-            print(origin == UIColor(red: 0.0, green: 0.0, blue: 0.0, alpha: 1.0))
-            print(one == UIColor(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0))
+            return colorMatrix
         }
+        return nil
     }
 }
