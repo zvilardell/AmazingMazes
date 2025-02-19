3dfc2d66808487bdf2056ebb7c4830607771b954
Zach Vilardell <zvilardell@bluebeam.com>
2025-02-18 23:35:49 -0500

    quick update part 2 - remove lingering pods data

--- AmazingMazes/Pods/Target Support Files/ImageScrollView/ImageScrollView-umbrella.h
+++
@@ -1,8 +0,0 @@
-#ifdef __OBJC__
-#import <UIKit/UIKit.h>
-#endif
-
-
-FOUNDATION_EXPORT double ImageScrollViewVersionNumber;
-FOUNDATION_EXPORT const unsigned char ImageScrollViewVersionString[];
-
--- AmazingMazes/Pods/Target Support Files/Pods-AmazingMazes/Pods-AmazingMazes-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_AmazingMazes : NSObject
-@end
-@implementation PodsDummy_Pods_AmazingMazes
-@end
